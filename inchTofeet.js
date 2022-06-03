53c87409d28ccf97da1aa505c524fdacef8b4741
nosianuri <nurinosia@gmail.com>
2022-06-03 10:54:36 +0600

    update

---
+++ inchTofeet.js
@@ -0,0 +1,27 @@
+// function inchToFeet(inches){
+//     var feet = inches / 12;
+//     return feet;
+// }
+// var myInches = 132;
+// var feet = inchesToFeet(myInches);
+// console.log('myInches in feet', feet);
+
+// var dadiInches = 144;
+// var feet = inchToFeet(dadiInches);
+// console.log(feet);
+
+// var naniInches = 156;
+// var feet = naniInches / 12;
+
+// var nanaInches = 168;
+// var feet = nanaInches / 12;
+
+
+// mile to km
+function mileTokilometer(miles){
+    var km = miles * 1.60934;
+    return km;
+}
+
+var marathon = mileTokilometer(26.2);
+console.log( 'marathon in km', marathon);
---
+++ recap.js
@@ -0,0 +1,53 @@
+// variable
+var time = '10:15pm';
+var bookPrice = 150;
+var isWhiteColor = false;
+
+// array
+var partners = ['sajid', 'mojid', 'nazmal', 'mozam'];
+var elementCount = partners.length;
+var nazmalIndex = partners.indexOf('nazmal');
+partners.push('nurul');
+partners.pop();
+
+
+// conditionals
+if(bookPrice < 120){
+    console.log
+}
+
+// loop
+var i =0;
+while(i <= 17){
+    // do some work
+    i++;
+}
+
+for(var i = 0; i<=17; i++){
+
+}
+
+
+// function
+
+function isMoonUp(time){
+    if(time >= 19 && time <= 5){
+        return true;
+    }
+    return false;
+}
+
+var moonStatus = isMoonUp(21);
+
+
+// let const
+
+// value of variable could change
+let price = 27;
+price = 29;
+price = 31;
+
+// value of the variable will not change
+let  myName = 'lal e lal mr. Helal';
+console.log(myName);
+myName = 22;
